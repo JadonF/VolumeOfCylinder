9d3ade80fae302c581707797c7507e7f6957743e
JadonF <36163291+JadonF@users.noreply.github.com>
2018-05-14 10:13:54 -0400

    Initial commit

---
+++ VolumeOfCylinder/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace VolumeOfCylinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(64, 25);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(128, 13);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Enter a radius and height.";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(12, 64);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 1;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(161, 63);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(93, 125);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Calculate";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ VolumeOfCylinder/Form1.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by: Jadon F
+ * Created on: 14-May-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #31 - Volume of a Cylinder
+ * This program calculates the volume of a cylinder
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolumeOfCylinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void calculateVolume(double radius, double height)
+        {
+            //local variables
+            double volume;
+            double faceArea;
+
+            //constants
+            const double PI = 3.14;
+
+            //calculate
+            faceArea = PI * Math.Pow(radius, 2);
+            volume = faceArea * height;
+
+            //message box
+            MessageBox.Show("The volume is " + volume + " units squared.", "Volume of a Cylinder");
+        
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //local variables
+            double userRadius;
+            double userHeight;
+
+            //assign variables
+            userRadius = Convert.ToDouble(txtRadius.Text);
+            userHeight = Convert.ToDouble(txtHeight.Text);
+
+            //call calculateVolume
+            calculateVolume(userRadius, userHeight);
+
+        }
+    }
+}
